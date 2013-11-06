3a51596072f9068a2bcc4a49cbf6037f55954b30
Nicolas Brousse <pro@nicolas-brousse.fr>
2013-11-06 03:53:21 +0100

    Move container and update application

--- HeyDoc/Application.php
+++ HeyDoc/Application.php
@@ -7,69 +7,39 @@ use dflydev\markdown\MarkdownExtraParser;
 use Symfony\Component\Yaml\Exception\ParseException;
 use Symfony\Component\Yaml\Yaml;
 
-class Application extends \Pimple
+class Application
 {
-    public function __construct($appBaseDir)
+    protected $container;
+
+    protected $page;
+
+    public function __construct($appBaseDir, Request $request)
     {
-        $c = $this;
-
-        $this['web_dir']  = realpath($appBaseDir);
-        $this['root_dir'] = realpath($c['web_dir'] . '/../');
-        $this['docs_dir'] = realpath($c['root_dir'] . '/docs/');
-
-        $this['configs'] = $this->share(function () use ($c) {
-            $settingsFilename = realpath($c['docs_dir'] . '/settings.yml');
-            // TODO return exception if file does not exists
-            return Yaml::parse(file_get_contents($settingsFilename));
-        });
-
-        $this['markdown_parser'] = $this->share(function () use ($c) {
-            return new MarkdownExtraParser();
-        });
-
-        $this['tree'] = $this->share(function () use ($c) {
-            return new Tree($c['docs_dir']);
-        });
-
-        $this['themes'] = $this->share(function () use ($c) {
-            $templates = $c['template_dirs'];
-            $templates[] = __DIR__.'/Resources/themes';
-
-            return new ThemeSet($templates);
-        });
-
-        $this['twig'] = $this->share(function () use ($c) {
-            $twig = new \Twig_Environment(new \Twig_Loader_Filesystem(array('/')), array(
-                'strict_variables' => true,
-                'debug'            => true,
-                'auto_reload'      => true,
-                'cache'            => false,
-            ));
-            $twig->addExtension(new TwigExtension());
-
-            return $twig;
-        });
-
-        /**
-         * Default configs
-         */
-        // $defaults = array(
-        //     'theme'         => 'enhanced',
-        //     'title'         => 'HeyDoc',
-        //     'template_dirs' => array(),
-        //     'cache_dir'     => getcwd() . '/cache',
-        // );
-        // $this['configs'] = array_replace($defaults, $this['configs']);
+        $this->container = new Container($appBaseDir);
+        $this->container['request'] = $request;
     }
 
     public function run()
     {
-        // TODO
-        $this->prepare();
+        try {
+            $this->prepare();
+            $this->render();
+        }
+        catch (NotFoundException $e) {
+            $response = new Response($e->getMessage(), 404);
+            $response->send();
+        }
     }
 
     protected function prepare()
     {
+        $this->page = $this->container->get('router')->process();
+    }
 
+    protected function render()
+    {
+
+        $response = new Response();
+        $response->send();
     }
 }
---
+++ HeyDoc/Container.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace HeyDoc;
+
+use dflydev\markdown\MarkdownExtraParser;
+
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Yaml;
+
+class Container extends \Pimple
+{
+    public function __construct($appBaseDir)
+    {
+        $c = $this;
+
+        $this['web_dir']  = realpath($appBaseDir);
+        $this['root_dir'] = realpath($c['web_dir'] . '/../');
+        $this['docs_dir'] = realpath($c['root_dir'] . '/docs/');
+
+        $this['configs'] = $this->share(function () use ($c) {
+            $settingsFilename = realpath($c['docs_dir'] . '/settings.yml');
+            // TODO return exception if file does not exists
+            return Yaml::parse(file_get_contents($settingsFilename));
+        });
+
+        $this['markdown_parser'] = $this->share(function () use ($c) {
+            return new MarkdownExtraParser();
+        });
+
+        $this['parser'] = $this->share(function () use ($c) {
+            return new Parsor($c);
+        });
+
+        $this['tree'] = $this->share(function () use ($c) {
+            return new Tree($c['docs_dir']);
+        });
+
+        $this['router'] = $this->share(function() use ($c) {
+            return new Router($c);
+        });
+
+        $this['themes'] = $this->share(function () use ($c) {
+            $templates = $c['template_dirs'];
+            $templates[] = __DIR__.'/Resources/themes';
+
+            return new ThemeSet($templates);
+        });
+
+        $this['twig'] = $this->share(function () use ($c) {
+            $twig = new \Twig_Environment(new \Twig_Loader_Filesystem(array('/')), array(
+                'strict_variables' => true,
+                'debug'            => true,
+                'auto_reload'      => true,
+                'cache'            => false,
+            ));
+            $twig->addExtension(new TwigExtension());
+
+            return $twig;
+        });
+
+        /**
+         * Default configs
+         */
+        // $defaults = array(
+        //     'theme'         => 'enhanced',
+        //     'title'         => 'HeyDoc',
+        //     'template_dirs' => array(),
+        //     'cache_dir'     => getcwd() . '/cache',
+        // );
+        // $this['configs'] = array_replace($defaults, $this['configs']);
+    }
+
+    public function get($name)
+    {
+        return $this[$name];
+    }
+}
