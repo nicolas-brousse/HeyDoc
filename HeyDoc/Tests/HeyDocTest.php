c980770d60ae69702e34b7e86d11dace466a2076
Nicolas Brousse <pro@nicolas-brousse.fr>
2013-11-07 01:25:15 +0100

    Config phpunit and travis

--- HeyDoc/Tests/HeyDocTest.php
+++ HeyDoc/Tests/HeyDocTest.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace HeyDoc;
+namespace HeyDoc\Tests;
+
+use HeyDoc\HeyDoc;
 
 class HeyDocTest extends \PHPUnit_Framework_TestCase
 {
-
-    public function testRun()
+    public function testVersion()
     {
-        // TODO
+        $this->assertEquals(HeyDoc::VERSION, '0.0.1-beta');
     }
 }
